52e895c650ce8b9f8df99b36f524995858517550
Kevin Walton <kevywalton@gmail.com>
2019-05-02 12:07:53 +0000

    Ran first test to fail & then set it to pass

---
+++ test_boggle.py
@@ -0,0 +1,10 @@
+import unittest
+
+class test_boggle(unittest.TestCase):
+    def test_is_this_thing_on(self):
+        # self.assertEqual(1, 0) 1st failed test
+        self.assertEqual(1, 1)
+
+
+
+        
